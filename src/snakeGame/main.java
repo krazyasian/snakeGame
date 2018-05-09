b18a3a6c9945760f24a2a7e619ad9e1d245837bf
husyy003 <31714898+husyy003@users.noreply.github.com>
2018-05-10 01:46:12 +0930

    Added Game's starting point
    Game starts from the main window

--- src/snakeGame/main.java
+++ src/snakeGame/main.java
@@ -3,8 +3,19 @@ package snakeGame;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+			Boolean existed = false;
+				System.out.println("Checking if Login details matches with"
+						+ " the database :) + IF TRUE then Start GameState");
+				
+				//change this one to true after checking with the database
+				existed = true;
+				System.out.println("Creating Player");
+				Player p=new Player("P1");
+				System.out.println("Starting Game");
+				GameState g=new GameState();
+				g.initGame();
+				
+				//** TODO :- Pass Moves to G here
+			}
 
 }
