8e8f3ea376e42cee82088a5825ff4a65a2aa16f4
husyy003 <31714898+husyy003@users.noreply.github.com>
2018-05-10 01:47:22 +0930

    cM
    cM

--- src/snakeGame/main.java
+++ src/snakeGame/main.java
@@ -1,14 +1,16 @@
 package snakeGame;
 
 public class main {
-
+static Boolean existed = false;
 	public static void main(String[] args) {
-			Boolean existed = false;
+			
 				System.out.println("Checking if Login details matches with"
 						+ " the database :) + IF TRUE then Start GameState");
 				
-				//change this one to true after checking with the database
+				//****TODO change this one to true after checking with the database
 				existed = true;
+		
+		
 				System.out.println("Creating Player");
 				Player p=new Player("P1");
 				System.out.println("Starting Game");
