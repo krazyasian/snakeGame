de6b06af24327b1ec401449c862819e42334c1d3
Albert Palada <palap002@mymail.unisa.edu.au>
2018-05-31 15:11:24 +0930

    Added the Server skeleton code
    Added the basic functions and properties needed in the server.

--- src/snakeGame/Server.java
+++ src/snakeGame/Server.java
@@ -1,5 +1,51 @@
 package snakeGame;
 
+import java.util.HashMap;
+
 public class Server {
+	private Buffer buffer;
+	private State gameState;
+	private HashMap playerStates;
+	
+	//CONSTRUCTOR
+	public Server () {
+		
+	}
+	
+	//pull moves from the buffer
+	private Buffer getBuffer () {
+		return buffer;
+	}
+	
+	//notify the players to update their game state
+	private void notifyPlayers () {
+		
+	}
+	
+	
+	
+	//updates the game interface with the new moves coming from the players
+	private synchronized void updateGameInterface () {
+		//
+		
+	}
+	
+	private synchronized Thread addThread () {
+		return new Thread();
+	}
+	
+	
+	private synchronized void removePlayer (Player removePlayer) {
+		playerStates.remove(removePlayer);
+	}
+	
+	//verify login player
+	private synchronized boolean login (Player loginPlayer) {
+		if (loginPlayer != null) {
+			return true;
+		}
+		
+		return false;
+	}
 
 }
